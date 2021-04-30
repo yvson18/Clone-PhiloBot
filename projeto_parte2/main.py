728dfb2ef88dc1d27f2a1e97d797fc6b9a1e95bc
Yvson <yvsonnunes7@gmail.com>
2021-04-30 14:44:16 -0300

    Manipulaçao mais Db

--- projeto_parte2/main.py
+++ projeto_parte2/main.py
@@ -1,15 +1,34 @@
 from PIL import Image, ImageFont,ImageDraw
 import textwrap
+import sqlite3
+import io
 
-frase_de_efeito = "\" " + "Vem monstro!" + " \"" # print(len(long_string)) nesse caso, o bot so aceita strings de no maximo 160 caracteres
+#------------------------------------------------ Consultando o banco---------------------------------------------------------------
+banco = sqlite3.connect("image_data_ultimate.db")
+
+cursor = banco.cursor()
+
+row =  cursor.execute("SELECT * FROM philosophers_table ORDER BY random() LIMIT 1") # retorna uma linha aleatória
+
+for x in row:
+    rec_im = x[2] # a terceira coluna da row indexada por dois eh a imagem
+    pensador = x[1] 
+
+autor_image = Image.open(io.BytesIO(rec_im))
+
+cursor.close()
+banco.close()
+
+#------------------------------------------------ Adicionando texto ---------------------------------------------------------------
+
+frase_de_efeito = "\" " + "Frase Randomicamente Engraçada" + " \"" # print(len(long_string)) nesse caso, o bot so aceita strings de no maximo 160 caracteres
 
 fundo = Image.open("imgs_req/fundo_preto.jpg")
 
-#------------------------------------------------ Adicionando texto ---------------------------------------------------------------
 draw = ImageDraw.Draw(fundo)
 
 text_formatado = textwrap.fill(frase_de_efeito,23) # segundo arg width
-autor = "-Leo Stronda"
+autor = "-" + pensador
 
 font1 = ImageFont.truetype("fonts/arial_narrow_7.ttf",50)
 font2 = ImageFont.truetype("fonts/arial_narrow_7.ttf",30)
@@ -28,9 +47,8 @@ def get_concat_h(im1, im2):
     dst.paste(im2, (im1.width, 0))
     return dst
 
-autor_image = Image.open("image_data/Leo_Stronda.jpg")
-autor_image_newsize = autor_image.resize((428,584)) #Redmensionar para encaixar
 
+autor_image_newsize = autor_image.resize((428,584)) #Redmensionar para encaixar
 
 parte_branca = Image.open("imgs_req/parte_branca_650_x_584.jpg")
 
@@ -40,7 +58,4 @@ mask = Image.open("imgs_req/gradiente.jpg").convert("L")
 
 img_final = Image.composite(imagem_concatenada,fundo_mod,mask)
 
-img_final.save("img_output/Imagem-Retornada-1.jpg")
-
 img_final.show()
-
